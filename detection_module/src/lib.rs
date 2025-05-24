bbf6fe1fc8163ff11ebeb59276194d1ba995f47a
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-24 22:57:29 +0200

    feat: added hashing file raw content

--- detection_module/src/lib.rs
+++ detection_module/src/lib.rs
@@ -1,18 +1,40 @@
 use pyo3::prelude::*;
 use rayon::prelude::*;
-
+use sha2::{Sha256, Digest};
+use std::io::{BufReader, Read};
+use std::fs::File;
 
 #[pyfunction]
-fn hash_file(path: String) -> Option<String> {
-    Ok(format!("Hashed: {}", text))
+fn hash_file(path: String) -> PyResult<Option<String>> {
+    let file = match File::open(&path) {
+        Ok(f) => f,
+        Err(_) => return Ok(None),
+    };
+    let mut reader = BufReader::new(file);
+    let mut hasher = Sha256::new();
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        hasher.update(&buffer[..count]);
+    }
+
+    Ok(Some(format!("{:x}", hasher.finalize())))
 }
 #[pyfunction]
 fn find_duplicates(hashes: Vec<(String, String)>) -> PyResult<Vec<Vec<String>>> {
+    let m = (0..2)
+        .map(|i| (0..2).map(|j| format!("({},{})", i, j)).collect())
+        .collect();
+    Ok(m)
 }
 
 #[pymodule]
 fn detection_module(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hash, m)?)?;
+    m.add_function(wrap_pyfunction!(hash_file, m)?)?;
     m.add_function(wrap_pyfunction!(find_duplicates, m)?)?;
     Ok(())
 }
